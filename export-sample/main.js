ba119b05a9b6296b35c8efe4c7a245c1cbf5120f
shinohara takuya <t_shinohara@sjc-inc.co.jp>
2023-03-22 14:36:18 +0900

    add bar

--- export-sample/main.js
+++ export-sample/main.js
@@ -2,10 +2,10 @@ import { data, Foo as Foo2 } from "./foo.js"
 
 console.log('--- main ---------------------');
 
-function hoge() { console.log('hoge') }
-
 document.getElementById('btn').addEventListener('click', _ => {
   Foo2();
   console.log(data);
   alert("クリックされました！ " + data);
 });
+
+export default function hoge() { console.log('hoge') };
