2551757654789ec97edc583c13945faacbeafb28
shinohara takuya <t_shinohara@sjc-inc.co.jp>
2023-03-22 13:46:12 +0900

    add hoge

--- export-sample/main.js
+++ export-sample/main.js
@@ -2,6 +2,8 @@ import { data, Foo as Foo2 } from "./foo.js"
 
 console.log('--- main ---------------------');
 
+function hoge() { console.log('hoge') }
+
 document.getElementById('btn').addEventListener('click', _ => {
   Foo2();
   console.log(data);
